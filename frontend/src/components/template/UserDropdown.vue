8e43fa99bbaab1f5752fb4f4e102563933561377
lucasCarvalhaes <lucascarber@gmail.com>
2021-02-16 10:39:56 -0300

    adicionado header e userdropdown

---
+++ frontend/src/components/template/UserDropdown.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="user-dropdown">
+        <div class="user-button">
+            <span class="d-none d-sm-block">{{ user.name }}</span>
+            <div class="user-dropdown-img">
+                <Gravatar :email="user.email" alt="User" />
+            </div>
+            <i class="fa fa-angle-down"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Gravatar from 'vue-gravatar'
+
+export default {
+    name: 'UserDropdown',
+    components: { Gravatar },
+    computed: mapState(['user'])
+}
+</script>
+
+<style>
+    .user-dropdown {
+        height: 100%;
+    }
+
+    .user-button {
+        display: flex;
+        align-items: center;
+        color: #fff;
+        font-weight: 100;
+        height: 100%;
+    }
+</style>
--- frontend/src/config/store.js
+++ frontend/src/config/store.js
@@ -5,7 +5,11 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        isMenuVisible: true
+        isMenuVisible: true,
+        user: {
+            name: 'Jo',
+            email: 'jo@jo.com.br'
+        }
     },
     mutations: {
         toggleMenu(state, isVisible) {
